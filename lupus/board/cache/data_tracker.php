aab6969318b37d4f657f2fd1c6ddaac6b6f39855
marwolf <Marwolf@users.noreply.github.com>
2011-10-16 04:20:44 +0000

    Attempted to correct cache CSS error.

--- lupus/board/cache/data_tracker.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1350273283
-331
-a:10:{s:17:"allow_attachments";s:1:"1";s:15:"attachment_path";s:13:"files/tracker";s:19:"default_status_type";s:1:"0";s:19:"enable_post_confirm";s:1:"1";s:15:"max_attachments";s:1:"5";s:14:"posts_per_page";s:2:"10";s:10:"send_email";s:1:"1";s:16:"tickets_per_page";s:2:"15";s:13:"top_reporters";s:2:"10";s:7:"version";s:5:"0.5.0";}
