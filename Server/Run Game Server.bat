771d96f0b50d7a7426a8d6b4cc491b286952850d
marwolf <marwolf@6b356873-48a9-4dbc-8488-63e82f667dd6>
2013-09-10 15:01:52 +0000

    Set IP connection information to localhost with username as "root" and password as "password". Updated build path in NetBeans to use Java 1.7

--- Server/Run Game Server.bat
+++ Server/Run Game Server.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jdk1.6.0_29/bin/java.exe" -classpath "dist\Lupus Regnum Server.jar" org.lupus_regnum.gs.Server > Game_Server_Log.txt 2>&1
+java -classpath "dist\Lupus Regnum Server.jar" org.lupus_regnum.gs.Server > Game_Server_Log.txt 2>&1
